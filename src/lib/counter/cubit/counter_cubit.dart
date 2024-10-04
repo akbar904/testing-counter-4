903910b50a1c0957b0f4bb6b53a17babbd4fbe43
zharfan-a <zharfan@walturn.com>
2024-10-04 07:37:14 +0700

    Update 30 files

--- src/lib/counter/cubit/counter_cubit.dart
+++ src/lib/counter/cubit/counter_cubit.dart
@@ -1,8 +1,9 @@
+
 import 'package:bloc/bloc.dart';
 
 class CounterCubit extends Cubit<int> {
-  CounterCubit() : super(0);
+	CounterCubit() : super(0);
 
-  void increment() => emit(state + 1);
-  void decrement() => emit(state - 1);
+	void increment() => emit(state + 2);
+	void decrement() => emit(state - 1);
 }
